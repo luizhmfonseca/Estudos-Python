7cdb50837e37f5b7697dc99fa0917342888d4101
LUIZ FONSECA <luiz.hmfonseca@gmail.com>
2026-02-20 18:00:38 -0800

    update
    teste

---
+++ AULA 07 - Operadores Aritméticos/exemplo03.py
@@ -0,0 +1,2 @@
+nome = input("Digite seu nome:")
+print("Prazer em te conhecer {:=^20}!" .format(nome))
--- EXERCÍCIOS - meus códigos/EX06.py
+++ EXERCÍCIOS - meus códigos/EX06.py
@@ -1,3 +1,5 @@
+# Exercício 06 - Operadores Aritméticos.
+# Faça um programa que leia um número inteiro e mostre na tela o seu dobro, triplo e raiz quadrada.
 n=int (input('insira um  número: '))
 print(n*2, 'o dobro')
 print(n*3, 'o triplo')
